6b90c71a736eb135b003277767d27d719f1baf67
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-05-09 16:31:12 +0100

    Test Save New Document

--- src/main/java/guru/springframework/reactivemongo/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/reactivemongo/services/BeerServiceImpl.java
@@ -1,12 +1,23 @@
 package guru.springframework.reactivemongo.services;
 
+import guru.springframework.reactivemongo.mappers.BeerMapper;
 import guru.springframework.reactivemongo.model.BeerDTO;
+import guru.springframework.reactivemongo.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Service
+@RequiredArgsConstructor
 public class BeerServiceImpl implements BeerService {
+    private final BeerMapper beerMapper;
+    private final BeerRepository beerRepository;
+
     @Override
-    public Mono<BeerDTO> saveBeer(BeerDTO beerDTO) {
-        return null;
+    public Mono<BeerDTO> saveBeer(Mono<BeerDTO> beerDTO) {
+        return beerDTO.map(beerMapper::beerDtoToBeer)
+                .flatMap(beerRepository::save) // stores in the DB
+                .map(beerMapper::beerToBeerDto);
     }
 
     @Override
